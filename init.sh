c7142f81103b6ebe4365f72a8cd437f6e7aa6aa1
Michael <47230631+cavanaghm@users.noreply.github.com>
2024-04-08 06:19:41 +1000

    sudo for go reinstall

--- init.sh
+++ init.sh
@@ -111,7 +111,7 @@ fi
 
 ## Install go
 echo 'Installing go'
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
 
